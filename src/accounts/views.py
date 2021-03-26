c0af644f091eccdcfd0fb56d6365b3259e57c730
erichamers <erichamers@gmail.com>
2021-03-25 22:47:14 -0300

    basic auth implemented

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .forms import UserCreationForm
+
+
+class RegisterView(CreateView):
+    template_name = 'accounts/register.html'
+    form_class = UserCreationForm
+
+    def get_success_url(self):
+        return reverse('website:index')
+
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'
+
+    def get_success_url(self) -> str:
+        return reverse('website:index')
